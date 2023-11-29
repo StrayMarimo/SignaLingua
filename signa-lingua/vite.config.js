c40fd1f06a6f729046eb626e65c6bdc1b9610afd
StrayMarimo <anbasbas@gbox.adnu.edu.ph>
2023-11-30 02:34:47 +0800

    feat: added ssl config

--- signa-lingua/vite.config.js
+++ signa-lingua/vite.config.js
@@ -1,11 +1,34 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+// 001 import the basicSsl Vite plugin
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+// 002 set the Laravel host and the port
+const host = "127.0.0.1";
+const port = "8000";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
+        // 003 load the basicSsl plugin
+        basicSsl(),
     ],
+    // 004 set the server section
+    server: {
+        // 005 enabling the HTTPnpm install @vitejs/plugin-basic-ssl S
+        https: true,
+        // 006 setting the proxy with Laravel as target (origin)
+        proxy: {
+            "^(?!(/@vite|/resources|/node_modules))": {
+                target: `http://${host}:${port}`,
+            },
+        },
+        host,
+        port: 5173,
+        // 007 be sure that you have the Hot Module Replacement
+        hmr: { host },
+    },
 });
